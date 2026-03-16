9813d722588350071548f98eee34a94d77dc5104
Agneya48 <josh.t.hampton@gmail.com>
2026-03-15 20:25:59 -0400

    Added NameController and associated Welcome view

---
+++ DemoGitHub/Controllers/NameController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoGitHub.Controllers
+{
+    // Josh Hampton - Very simple example controller that takes a name as a parameter,
+    // and passes it to the view using ViewBag.
+    public class NameController : Controller
+    {
+        public IActionResult Welcome(string name)
+        {
+            ViewBag.Name = name;
+            return View();
+        }
+    }
+}
